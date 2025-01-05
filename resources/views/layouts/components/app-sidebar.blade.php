40aef09d7419c9bd120e416ea051051b002906fa
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-01-05 21:24:23 +0530

    push

--- resources/views/layouts/components/app-sidebar.blade.php
+++ resources/views/layouts/components/app-sidebar.blade.php
@@ -2,10 +2,10 @@
 				<div class="sticky">
 					<aside class="app-sidebar">
 						<div class="main-sidebar-header active">
-							<a class="header-logo active" href="{{url('index')}}">
+							<a class="header-logo active" href="{{url('/')}}">
 								<img src="{{asset('assets/img/brand/logo.png')}}" class="main-logo  desktop-logo" alt="logo">
 								<img src="{{asset('assets/img/brand/logo-white.png')}}" class="main-logo  desktop-dark" alt="logo">
-								<img src="{{asset('assets/img/brand/favicon.png')}}" class="main-logo  mobile-logo" alt="logo">
+								<img src="{{asset('assets/img/brand/logo.png')}}" class="main-logo  mobile-logo"  alt="logo">
 								<img src="{{asset('assets/img/brand/favicon-white.png')}}" class="main-logo  mobile-dark" alt="logo">
 							</a>
 						</div>
